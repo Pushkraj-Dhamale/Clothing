509e43b1cf578d2123c351f2aa20e0c52110cfb4
Ricky Parte <77445318+RickyParte@users.noreply.github.com>
2023-10-28 20:26:27 +0530

    Update OrderCard.jsx
    Update OrderCard

--- frontend/src/components/OrderCard.jsx
+++ frontend/src/components/OrderCard.jsx
@@ -20,6 +20,7 @@ const OrderCard = ({ orderId }) => {
             .then((result) => {
                 setProducts(result.order.products);
                 setDate(result.order.orderDate);
+                console.log(result.order.orderDate)
                 if (result.order.prepare) {
                     setOrderStatus("Order is preparing.");
                 } else if (result.order.onWay) {
@@ -90,13 +91,7 @@ const OrderCard = ({ orderId }) => {
                     }
                 </Box>
                 <Divider />
-                <SimpleGrid my={3} columns={{ base: 1, sm: 2, md: 3, lg: 4 }} spacing={3} >
-                    {
-                        products.map((product, index) => {
-                            return product !==null && <ClothesCard key={index} productId={product} isDelivered={orderStatus === "Order has been delivered."} />
-                        })
-                    }
-                </SimpleGrid>
+                
             </Box>
             <ReportModal onClose={onClose} isOpen={isOpen} orderId={orderId} />
         </>
